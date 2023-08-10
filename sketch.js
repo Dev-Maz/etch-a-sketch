2d5687b9bd995465357ad48391bc454faec561ac
Maziar-M <maziar.mir.2002@gmail.com>
2023-08-10 22:35:58 +0330

    Add a menu for more interactivity
    Added reset and change buttons for the sketch board to the menu

--- sketch.js
+++ sketch.js
@@ -1,6 +1,25 @@
+let currentSize;
 let penColor = 'black-pen';
 let mouseIsDown = false;
+const sketchBoard = document.querySelector('#sketch-board');
+createSketchBoard(50);
 
+// Menu functionality
+const resetSketchBoardBtn = document.querySelector('#erase-all');
+resetSketchBoardBtn.addEventListener('click', () => {
+    sketchBoard.innerHTML = '';
+    createSketchBoard(currentSize);
+});
+
+const newSketchBoardBtn = document.querySelector('#new-sketch');
+newSketchBoardBtn.addEventListener('click', () => {
+    sketchBoard.innerHTML = '';
+    let userSizeChoice = prompt('What should the new sketch board\'s size be?');
+    createSketchBoard(userSizeChoice);
+})
+
+
+// Function definitions
 function createSketchBoard(girdSize) {
     // Optimization check
     if (girdSize > 100) {
@@ -8,8 +27,9 @@ function createSketchBoard(girdSize) {
         alert('The maximum size of the sketch board is 100 for optimization purposes. A sketch board of size 100 was created.');
     }
 
+    currentSize = girdSize;
+
     // Sketch board creation
-    const sketchBoard = document.querySelector('#sketch-board');
     for (let i = 0; i < girdSize; i++) {
         const row = document.createElement('div');
         row.classList.add('row');
